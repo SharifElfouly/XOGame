f2e5913daf56ea9fe862e9c46bd0aaa5bd5235f3
Sharif Elfouly <selfouly@gmail.com>
2017-12-16 16:09:05 +0100

    changed the activity to be fullscreen

--- app/src/main/java/shafou/xospiel/Activities/GameActivity.java
+++ app/src/main/java/shafou/xospiel/Activities/GameActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import shafou.xospiel.GameLogic.XOGame;
 
@@ -24,12 +26,24 @@ public class GameActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setFullscreen();
+
         Intent gameMenuIntent = getIntent();
         int fieldSize = gameMenuIntent.getIntExtra("field_size", 3);
         int tokens_to_win = gameMenuIntent.getIntExtra("tokens_to_win", 3);
 
         View gameView = XOGame.getInstance(this, fieldSize, tokens_to_win).spielfeld();
         setContentView(gameView);
+    }
+
+    /**
+     * Sets activity to fullscreen.
+     */
+    private void setFullscreen() {
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 
 }
